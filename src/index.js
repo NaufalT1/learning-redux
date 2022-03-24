a96bf1668a5f9739c1abe88a59c927714918563b
Naufal Ibrahim <naufal.ti@outlook.com>
2022-03-24 14:17:17 +0700

    Use fake api instead

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import store from "./store/store";
 import { addProduct, markAsDiscount, removeProduct } from "./store/product";
 import { addProductToCart, removeProductFromCart } from "./store/cart";
 
-
 // store.subscribe(() => {
 //     console.log("store changed", store.getState());
 // });
@@ -16,22 +15,22 @@ import { addProductToCart, removeProductFromCart } from "./store/cart";
 //     name: "iPhone XS",
 //     price: 1000,}));
 
-
 // store.dispatch(addProduct({
 //     name: "iPhone XS Max",
 //     price: 1000,}));
-    
+
 // store.dispatch(removeProduct(1));
 // store.dispatch(markAsDiscount(2));
 
 // store.dispatch(addProductToCart(2, 2));
 
-
 //API CONSUME
 store.dispatch({
-    type: 'apiRequest', //3.1 Create Action Creators
-    payload: {
-        url: '/products',
-        method: 'get'
-    }
+  type: "apiRequest", //3.1 Create Action Creators
+  payload: {
+    url: "https://jsonplaceholder.typicode.com/users",
+    method: "GET",
+    onSuccess: "onsuccess",
+    onError: "onerror",
+  },
 });
